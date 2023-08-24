634482299bb55e559929c4779b730ca5564d1133
OLEKSANDR KUROCHKIN <kurochkinac@gmail.com>
2023-08-24 11:18:00 +0300

    Fix search

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,61 +1,67 @@
 import React, { useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
-
 import { resultSearch } from '../../services/fetch';
+import css from './Movies.module.css';
 
-import css from './Movies.module.css'
+function MovieList({ movies }) {
+  return (
+    <ul>
+      {movies.map(({ id, title }) => (
+        <li key={id} className={css.searchList}>
+          <Link to={`/movies/${id}`}>{title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default function Movies() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [foundMovies, setFoundMovies] = useState([]);
   const [status, setStatus] = useState('');
-  const name = searchParams.toString() || '';
 
-  const checkSearchParam = evt => {
-    const checkParam =
-      evt.target.value !== '' ? { movie: evt.target.value } : {};
-    setSearchParams(checkParam);
+  const query = searchParams.get('query') || '';
+
+  const checkSearchParam = (evt) => {
+    const inputValue = evt.target.value;
+    setSearchParams({ query: inputValue });
   };
 
   const getMovies = () => {
-    resultSearch({ query: name })
-      .then(data => setFoundMovies(data.results))
-      .catch(() => {
-        setStatus('error');
-      });
+    if (query) {
+      
+      resultSearch(query)
+        .then((data) => setFoundMovies(data.results))
+        .catch(() => {
+          setStatus('An error occurred while fetching data.');
+        });
+    }
   };
 
-  const markup =
-    foundMovies.length > 0
-      ? foundMovies.map(({ id, title }) => {
-          return (
-            <li key={id} className={css.searchList}>
-              <Link to={`/movies/${id}`}>{title}</Link>
-            </li>
-          );
-        })
-      : "";
+  const handleKeyPress = (evt) => {
+    if (evt.key === 'Enter') {
+      evt.preventDefault()
+      getMovies();
+    }
+  };
 
   return (
     <div className={css.searchBox}>
       <form>
         <input
-        className={css.searchInput}
+          className={css.searchInput}
           type="text"
           placeholder="Search the movie"
           name="search"
           onChange={checkSearchParam}
+          onKeyPress={handleKeyPress}
+          value={query}
         />
-        <button
-          type="button"
-          onClick={() => {
-            getMovies();
-          }}
-        >
+        <button type="button" onClick={getMovies}>
           Search
         </button>
       </form>
-      {status !== 'error' ? markup : <p>Not found</p>}
+      {status ? <p>{status}</p> : foundMovies.length > 0 ? <MovieList movies={foundMovies} /> : ""}
     </div>
   );
 }
